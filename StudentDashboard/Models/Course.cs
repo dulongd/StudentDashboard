13033ed3b6ec1562e1385ee9ce7fe258f9bacdbd
Danielle DuLong <danielledulong@gmail.com>
2021-11-11 18:24:20 -0500

    Create Student and Course Classes

---
+++ StudentDashboard/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentDashboard.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string CourseName { get; set; }
+
+        [Required]
+        public string CourseDescription { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string TutorName { get; set; }
+
+        [Required]
+        [Range(1,10)]
+        public int CourseRating { get; set; }
+    }
+}
---
+++ StudentDashboard/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentDashboard.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+
+        [Required]
+        public DateTime EnrolledDate { get; set; }
+
+        [Required]
+        public int Status { get; set; }
+
+        [Required]
+        [StringLength(10)]
+
+        public string Grade { get; set; }
+
+        public static readonly int inProgress = 0;
+        public static readonly int Completed = 1;
+    }
+}
